b1caf4ccc5e8ef073903dc48c03e4a00b9876576
rafaelpsouzaa <marksmancicone@gmail.com>
2021-04-27 21:22:45 -0300

    trocando app para social

--- javaBasico/meusite/src/index.js
+++ javaBasico/meusite/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Social from './social';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Social />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ javaBasico/meusite/src/social.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Social(){
+    return <h1>ola mundo 2</h1>
+}
+
+export default Social;
