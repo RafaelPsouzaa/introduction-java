c4fe138be6ad170e64d35d77cf7006063431a315
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2021-04-29 09:37:52 -0300

    funcao aumentar e diminuir

--- javaBasico/meusite/src/App.js
+++ javaBasico/meusite/src/App.js
@@ -1,25 +1,44 @@
 import React,{Component} from 'react';
 
-class Equipe extends Component{
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            nome: 'Rafael',
+            contador:0 
+        };
+
+        this.aumentar = this.aumentar.bind(this);
+    }
+
+    aumentar(){
+        let state = this.state;
+       state.contador += 1;
+
+       this.setState(state);
+    }
+    diminuir(){
+        
+    }
+
     render(){
         return(
             <div>
-                <h2>Funcionou!</h2>
+                <h1>Contador</h1>
+                
+               <h3>
+               <button onClick ={this.diminuir}>-</button>
+                 {this.state.contador}
+               <button onClick = {this.aumentar}>+ </button>
+               </h3>
             </div>
         );
     }
+
+
 }
 
 
-function App(){
 
-    return(
-        <div>
-            <h1>conhecao nossa equipe</h1>
-            <Equipe/>
-        </div>
-
-    );
-}
 
  export default App;
