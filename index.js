e8ddb4b65eb030d2cce0338b8d85639c6aa82383
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2021-04-29 08:18:57 -0300

    delete all

--- index.js
+++
@@ -1,5 +0,0 @@
-const lista = [1,2,3,4,5];
-const find = lista.find(function(item){
-    return item === 5
-});
-console.log(find);
--- javaBasico/aula6.java
+++
@@ -1,5 +0,0 @@
-public class aula6 {
-    public static void main(String[] args) {
-        System.out.println("you write something "+ args[0]);
-    }
-}
