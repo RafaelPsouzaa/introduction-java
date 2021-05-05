baddffe2e7380d84b057ac5f8aea6e68d45c5f35
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2021-05-05 08:24:58 -0300

    base

--- javaBasico/meusite/src/App.js
+++ javaBasico/meusite/src/App.js
@@ -1,27 +1,13 @@
-import React,{Component} from 'react';
+import React,{useState} from 'react';
+import styled from 'syled-components';
 
-class App extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            hora:'00.00.00'
-        };
-    }
-    componentDidMount(){
-        setInterval(()=>{
-            this.setState({hora: new Date().toLocaleTimeString() })
-        },1000);
-    }
+function App(){
 
-    render(){
-        return(
-            <div>
-                <h1>Meu Projeto {this.state.hora}</h1>
-            </div>
+return(
 
-        );
-    }
-    
+
+
+);
+
 }
-
 export default App;
