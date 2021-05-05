e5e4daf9745c9dfd856eb4cb90bfd162797b6ac9
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2021-05-05 11:03:36 -0300

    calculadora gorjeta

--- javaBasico/meusite/src/App.js
+++ javaBasico/meusite/src/App.js
@@ -1,13 +1,31 @@
 import React,{useState} from 'react';
-import styled from 'syled-components';
+import styled from 'styled-components';
 
-function App(){
+        const Input = styled.input`
+        width:200px;
+        height:30px;
+        font-size:16px;
+        padding:10px;
+        border:1px solid #000;
+        `;
+    function App(){
+        const [valor,setValor] = useState('');
+        const [gorjeta,setGorjeta] = useState('');
+        
+        const handleInput = (e) =>{
+            setValor(e.target.value)
+        };
+        const gorjeInput =(e) =>{
+            setGorjeta(e.target.value)
+        };
 
-return(
-
-
-
-);
-
-}
+        return (
+        <>
+        <h1>Calculadora Gorjeta</h1>
+        <div><h1>valor conta</h1></div><br/>
+        <Input type="text" value ={valor} onChange={handleInput}/><br/>
+        <br/> <Input type= "text" value ={gorjeta} onChange={gorjeInput}/>
+        </> 
+         );
+        }
 export default App;
